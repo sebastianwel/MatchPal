ccb13f3a573e93095763b970440622f9b2131f6d
Sebastian Welzer <sebastianwelzerx@gmail.com>
2023-06-19 10:06:48 +0200

    removed template components

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ import SubHeading from "../components/SubHeading";
 export default function Home() {
   return (
     <main>
-      <Heading>🐬 Capstone Template 🐬</Heading>
-      <SubHeading>We dolphinitely love coding!</SubHeading>
     </main>
   );
 }
