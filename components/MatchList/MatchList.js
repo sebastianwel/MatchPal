3157c132ca4342f0d703db1ef286574389d92dba
Sebastian Welzer <sebastianwelzerx@gmail.com>
2023-06-19 12:02:15 +0200

    add MatchCard Component and mate some changes to MatchList and teams-data

--- components/MatchList/MatchList.js
+++ components/MatchList/MatchList.js
@@ -1,8 +1,31 @@
 import styled from "styled-components";
+import { matches } from "../../lib/mock-data/matches";
+import { teams } from "../../lib/mock-data/teams";
+import MatchCard from "../MatchCard/MatchCard";
 
 
-export default function MatchList(){
+export default function MatchList(){   
+
+//used the matches mock-data and the teams mock-data to create a new array combining these two data-arrays to include the team names in the matches
+const matchesWithTeamNames = matches.map((match) => (
+    {...match, homeTeam: {name: teams.find((team) => team.id === match.homeTeamId).name, logoColor: teams.find((team) => team.id === match.homeTeamId).logoColor}, awayTeam:{name: teams.find((team) => team.id === match.awayTeamId).name, logoColor: teams.find((team) => team.id === match.awayTeamId).logoColor}}
+))
+
+console.log(matchesWithTeamNames)
+
     return(
+        <>
         <h1>Match Overview</h1>
+        <MatchUl>
+        {matchesWithTeamNames.map(({id, homeTeam, awayTeam, date, time}) => (
+            <MatchCard key={id} homeTeam={homeTeam.name} awayTeam={awayTeam.name} date={date} time={time} homeTeamColor={homeTeam.logoColor} awayTeamColor={awayTeam.logoColor}/>
+        ))}
+        </MatchUl>
+        </>
+
     )
 }
+
+const MatchUl = styled.ul`
+padding: 0;
+`
