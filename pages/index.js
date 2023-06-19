e29a42d0a5fb5dd6fffb84dabf5ad4c31fa08a70
Sebastian Welzer <sebastianwelzerx@gmail.com>
2023-06-19 14:33:19 +0200

    add footer

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import MatchList from "../components/MatchList";
 import AppHeader from "../components/AppHeader/AppHeader";
+import AppFooter from "../components/AppFooter/AppFooter";
 
 export default function Home() {
   return (
     <>
     <AppHeader/>
-    <main>
-      <MatchList/>
-    </main>
+      <main>
+        <MatchList/>
+      </main>
+    <AppFooter/>
     </>
   );
 }
