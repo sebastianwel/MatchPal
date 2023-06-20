af9840bdcaf075da3b8e4973e8f5a06479d61e9c
Sebastian Welzer <sebastianwelzerx@gmail.com>
2023-06-20 10:25:36 +0200

    finalized first version of match-details-page

--- pages/matches/[id].js
+++ pages/matches/[id].js
@@ -1,10 +1,13 @@
 import { useRouter } from "next/router"
 import { matches } from "../../lib/mock-data/matches";
 import { teams } from "../../lib/mock-data/teams";
+import { bars } from "../../lib/mock-data/bars";
+import { barsInMatches } from "../../lib/mock-data/barsInMatches";
 import styled from "styled-components";
 import AppHeader from "../../components/AppHeader/AppHeader";
 import AppFooter from "../../components/AppFooter/AppFooter";
 import SelectedMatch from "../../components/SelectedMatch";
+
 
 
 export default function MatchDetails(){
@@ -12,18 +15,63 @@ const router = useRouter();
 const {id} = router.query;
 
 const matchesWithTeamNames = matches.map((match) => (
-    {...match, homeTeam: {name: teams.find((team) => team.id === match.homeTeamId).name, logoColor: teams.find((team) => team.id === match.homeTeamId).logoColor}, awayTeam:{name: teams.find((team) => team.id === match.awayTeamId).name, logoColor: teams.find((team) => team.id === match.awayTeamId).logoColor}}
+    {...match, 
+        homeTeam: {name: teams.find((team) => team.id === match.homeTeamId).name, 
+        logoColor: teams.find((team) => team.id === match.homeTeamId).logoColor}, 
+        awayTeam:{name: teams.find((team) => team.id === match.awayTeamId).name, 
+        logoColor: teams.find((team) => team.id === match.awayTeamId).logoColor}}
 ))
+
+//use the current id of the router to find the fitting match
 const currentMatch = matchesWithTeamNames.find((match) => (
     match.id === parseInt(id)
 ))
+
+//filter the bars, which contain the id of the current match
+const currentBarsIds = currentMatch ? (barsInMatches.filter((bar) => (bar.gameIds.includes(currentMatch.id)))) : null
+
+//for the details page, only the bar name is necessary, so I created an array containing only the bar-name and id
+const barNames = bars.map((bar) => ({name: bar.name, id: bar.id}))
+
+//to find out, which bars show the current match, I extended the barsInMatches array with the bar name
+const barsShowingMatch = currentMatch ? currentBarsIds.map((currentBar) => (
+    {...currentBar, name: barNames.find((bar) => bar.id === currentBar.barId).name}
+)) : null 
+
+console.log(barsShowingMatch)
+
     return(
         <>
         <AppHeader/>
-        <button style={{marginTop: 55}}>ᐸ</button>
-        <SelectedMatch date={currentMatch.date} time={currentMatch.time} homeTeam={currentMatch.homeTeam.name} homeTeamLogoColor={currentMatch.homeTeam.logoColor} awayTeam={currentMatch.awayTeam.name} awayTeamLogoColor={currentMatch.awayTeam.logoColor}/>
+        <Button onClick={() => router.push("/")}>←</Button>
+        {currentMatch ? <SelectedMatch date={currentMatch.date} time={currentMatch.time} homeTeam={currentMatch.homeTeam.name} homeTeamLogoColor={currentMatch.homeTeam.logoColor} awayTeam={currentMatch.awayTeam.name} awayTeamLogoColor={currentMatch.awayTeam.logoColor}/> : <h2>loading</h2>}
+        <h3 style={{marginLeft: 10}}>Folgende Bars zeigen das Spiel:</h3>
+        <List>
+            {barsShowingMatch?.map((bar) => (
+                <ListItem key={bar.barId}>{bar.name}</ListItem>
+            ))}
+        </List>
 
         <AppFooter/>
         </>
     )
 }
+
+const Button = styled.button`
+margin-top: 55px;
+font-size: 20px;
+background: none;
+border: none;
+`
+
+const List = styled.ul`
+padding-left: 0px;
+`
+
+const ListItem = styled.li`
+list-style: none;
+border: 1px solid;
+border-radius: 10px;
+margin: 10px;
+padding: 10px;
+`
