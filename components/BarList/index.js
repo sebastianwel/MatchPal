9c430a97198c26c4991031de72cf6ccdbed78ddc
Sebastian Welzer <sebastianwelzerx@gmail.com>
2023-06-21 12:44:21 +0200

    delete styled components since not used here anymore

--- components/BarList/index.js
+++ components/BarList/index.js
@@ -22,29 +22,3 @@ list-style: none;
 padding-left: 0px;
 margin-bottom: 50px;
 `
-const ListItem = styled.li`
-list-style: none;
-display: flex;
-flex-direction: column;
-justify-content: space-around;
-margin: 5px;
-border: 1px solid;
-border-radius: 10px;
-padding-left: 10px;
-`
-
-const MatchPreviewSection = styled.div`
-display: flex;
-justify-content: flex-start;
-align-items: center;
-font-size: 10px;
-gap: 30px;
-`
-
-const MatchPreview = styled.div`
-display: flex;
-justify-content: space-around;
-align-items: center;
-gap: 10px;
-font-size: 10px;
-`
