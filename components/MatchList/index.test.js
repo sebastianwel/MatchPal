d2979abca1cc6e556f535797659957beec7e3364
Sebastian Welzer <sebastianwelzerx@gmail.com>
2023-06-21 12:36:36 +0200

    needed to fix the test

--- components/MatchList/index.test.js
+++ components/MatchList/index.test.js
@@ -2,7 +2,12 @@ import MatchList from ".";
 import { render, screen } from "@testing-library/react";
 
 test("test", () => {
-  render(<MatchList/>);
+  const matches = [
+    { id: 1, homeTeam: { name: "Team A", logoColor: "#123456" }, awayTeam: { name: "Team B", logoColor: "#789012" }, date: "2023-05-18", time: "18:00" },
+    { id: 2, homeTeam: { name: "Team C", logoColor: "#345678" }, awayTeam: { name: "Team D", logoColor: "#901234" }, date: "2023-05-19", time: "20:00" },
+  ];
+
+  render(<MatchList matches={matches}/>);
   const element = screen.getByText("Match Overview");
   expect(element).toBeInTheDocument();
 });
