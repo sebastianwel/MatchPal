7e0c0319eeb6bba59f1225c578e96336706b9b83
Sebastian Welzer <sebastianwelzerx@gmail.com>
2023-06-27 16:11:52 +0200

    did some refactoring

---
+++ components/ReviewCard/index.js
@@ -0,0 +1,33 @@
+import ReviewStar from "../../assets/ReviewStar";
+import styled from "styled-components";
+
+export default function ReviewCard({ username, rating, comment }) {
+  const renderReviewStars = (filledStars) => {
+    const stars = [];
+
+    for (let counter = 1; counter <= 5; counter++) {
+      const isSelected = counter <= filledStars;
+      stars.push(<ReviewStar key={counter} isSelected={isSelected} />);
+    }
+
+    return stars;
+  };
+
+  return (
+    <ReviewCardContainer>
+      <li>
+        <p>{username}</p>
+        <div>{renderReviewStars(rating)}</div>
+        <p>{comment}</p>
+      </li>
+    </ReviewCardContainer>
+  );
+}
+
+const ReviewCardContainer = styled.div`
+  border: 1px solid;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  border-radius: 10px;
+  padding: 5px;
+`;
--- components/ReviewsList/index.js
+++ components/ReviewsList/index.js
@@ -3,6 +3,7 @@ import ReviewStar from "../../assets/ReviewStar";
 import ReviewForm from "../ReviewForm";
 import { useEffect, useState } from "react";
 import { uid } from "uid";
+import ReviewCard from "../ReviewCard";
 
 export default function ReviewsList({ currentBar }) {
   const [reviews, setReviews] = useState(currentBar?.reviews);
@@ -11,40 +12,7 @@ export default function ReviewsList({ currentBar }) {
     setReviews(currentBar?.reviews || []);
   }, [currentBar?.reviews]);
 
-  const [rating, setRating] = useState(0);
-
-  function handleStarRating(value) {
-    setRating(value);
-  }
-  const maxStars = 5;
-  function renderReviewStars(filledStars) {
-    const stars = [];
-
-    //create a loop which defines how many stars are filled
-    for (let counter = 1; counter <= maxStars; counter++) {
-      //create the isSelected variable which is used as a prop in the stars-component
-      const isSelected = counter <= filledStars;
-      //now push a whole component into the array, to have a amount of "selected-stars" to render. Since the loop will iterate for 5-times, the difference in the array will be rendered as "!isSelected"
-      stars.push(<ReviewStar key={counter} isSelected={isSelected} />);
-    }
-
-    return stars;
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const data = new FormData(event.target);
-    const formData = Object.fromEntries(data);
-    //since I am not using a form-field for the stars, I need to copy the form-data and add the value from the selected stars into the copy
-    const formDataWithRating = { ...formData, rating: parseInt(rating) };
-
-    const newReview = {
-      id: uid(),
-      username: formDataWithRating.username,
-      comment: formDataWithRating.comment,
-      rating: formDataWithRating.rating,
-    };
-
+  function handleAddReview(newReview) {
     setReviews((prevReviews) => [...prevReviews, newReview]);
   }
 
@@ -53,20 +21,15 @@ export default function ReviewsList({ currentBar }) {
       <h4>So gefiel es anderen Nutzern:</h4>
       <Reviews>
         {reviews?.map((review, index) => (
-          <ReviewCardContainer key={index}>
-            <li>
-              <p>{review.username}</p>
-              <div>{renderReviewStars(review.rating)}</div>
-              <p>{review.comment}</p>
-            </li>
-          </ReviewCardContainer>
+          <ReviewCard
+            key={index}
+            username={review.username}
+            rating={review.rating}
+            comment={review.comment}
+          />
         ))}
       </Reviews>
-      <ReviewForm
-        onSubmit={handleSubmit}
-        rating={rating}
-        handleStarRating={handleStarRating}
-      />
+      <ReviewForm onAddReview={handleAddReview} />
     </ReviewsContainer>
   );
 }
